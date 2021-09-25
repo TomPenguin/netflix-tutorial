5e0205648fb44777df554a6db0b477b5be5f5d75
TomPenguin <tom.penguin.zoo@gmail.com>
2021-09-25 15:53:23 +0900

    add Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css"
 import endpoints from "./endpoints"
-import Banner from "./components/Banner"
-import Row from "./components/Row"
+import Nav from "components/Nav"
+import Banner from "components/Banner"
+import Row from "components/Row"
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row title="Top Rated" endpoint={endpoints.fetchTopRated} />
       <Row title="Horror Movies" endpoint={endpoints.fetchHorrorMovies} />
